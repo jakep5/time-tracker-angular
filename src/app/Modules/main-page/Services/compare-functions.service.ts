433b998c2c925061a5badbdf30f8d1ea28caefce
jakep5 <jakepagel1@yahoo.com>
2020-05-13 18:22:05 -0500

    work on sort direction implementation

--- src/app/Modules/main-page/Services/compare-functions.service.ts
+++ src/app/Modules/main-page/Services/compare-functions.service.ts
@@ -7,51 +7,78 @@ export class CompareFunctionsService {
 
   constructor() { }
 
+  private sortDirection: string = 'desc';
+
+
   compareName(a, b) {
-    if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
-    if (b.name.toLowerCase() > a.name.toLowerCase()) return -1;
+    if (this.sortDirection === 'desc') {
+      if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+      if (b.name.toLowerCase() > a.name.toLowerCase()) return -1;
+    }
+
+    if (this.sortDirection === 'asc') {
+      if (a.name.toLowerCase() > b.name.toLowerCase()) return -1;
+      if (b.name.toLowerCase() > a.name.toLowerCase()) return 1;
+    }
+   
   }
 
-  private sortDirection: string = 'asc';
 
   comparePriority(a, b) {
     let prioA = a.priority;
     let prioB = b.priority;
-
-    if (prioA === 'low' && prioB === 'med') {
-      return 1;
-    } else if (prioA === 'med' && prioB === 'high') {
-      return 1;
-    } else if (prioA === 'low' && prioB === 'high') {
-      return 1;
-    } else if (prioA === 'med' && prioB === 'low') {
-      return -1;
-    } else if (prioA === 'high' && prioB === 'low') {
-      return -1;
-    } else {
-      return -1;
-    }
-  }
-
-  compareHours(a, b) {
-    if (this.sortDirection === 'asc') {
-      if (a.hours > b.hours) {
-        return -1;
-      }
-      if (a.hours < b.hours) {
-        return 1;
-      }
-    }
-
     if (this.sortDirection === 'desc') {
-      if (a.hours > b.hours) {
+      if (prioA === 'low' && prioB === 'med') {
         return 1;
-      }
-      if (a.hours < b.hours) {
+      } else if (prioA === 'med' && prioB === 'high') {
+        return 1;
+      } else if (prioA === 'low' && prioB === 'high') {
+        return 1;
+      } else if (prioA === 'med' && prioB === 'low') {
         return -1;
+      } else if (prioA === 'high' && prioB === 'low') {
+        return -1;
+      } else {
+        return -1;
+      }
+    }
+
+    if (this.sortDirection === 'asc') {
+      if (prioA === 'low' && prioB === 'med') {
+        return -1;
+      } else if (prioA === 'med' && prioB === 'high') {
+        return -1;
+      } else if (prioA === 'low' && prioB === 'high') {
+        return -1;
+      } else if (prioA === 'med' && prioB === 'low') {
+        return 1;
+      } else if (prioA === 'high' && prioB === 'low') {
+        return 1;
+      } else {
+        return 1;
       }
     }
     
+  }
+
+  compareHours(a, b) {
+    if (this.sortDirection === 'desc') {
+      if (a.hours > b.hours) {
+        return -1;
+      }
+      if (a.hours < b.hours) {
+        return 1;
+      }
+    }
+
+    if (this.sortDirection === 'asc') {
+      if (a.hours > b.hours) {
+        return 1;
+      }
+      if (a.hours < b.hours) {
+        return -1;
+      }
+    }
   }
 
   changeSortDirection(direction: string) {
