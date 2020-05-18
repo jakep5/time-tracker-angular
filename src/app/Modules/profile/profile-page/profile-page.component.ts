68d40944a2859dc1e81f324e7e7ae680eaa71b40
jakep5 <jakepagel1@yahoo.com>
2020-05-18 14:54:22 -0500

    add total hours calculation and display

--- src/app/Modules/profile/profile-page/profile-page.component.ts
+++ src/app/Modules/profile/profile-page/profile-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../shared/Services/user.service';
+import { Task } from '../../shared/models/Task';
+import { TaskService } from '../../shared/Services/task-service.service';
 
 @Component({
   selector: 'app-profile-page',
@@ -9,13 +11,27 @@ import { UserService } from '../../shared/Services/user.service';
 export class ProfilePageComponent implements OnInit {
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private taskService: TaskService
   ) { }
 
   currentUser: string = localStorage.getItem('currentUser');
+  
+  tasks: Array<Task> = this.taskService.getTasks(sessionStorage.getItem('userId'))
 
+  userLevel: string;
+
+  userHours: number = 0;
+  
   ngOnInit(): void {
+    this.tasks = this.taskService.getTasks(sessionStorage.getItem('userId'))
+      .then(tasks => {
+        this.userLevel = this.userService.calculateUserLevel(tasks)
+        this.userHours = this.userService.calculateTotalHours(tasks);
+      })
 
   }
+
+
 
 }
--- src/app/Modules/shared/Services/user.service.ts
+++ src/app/Modules/shared/Services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Subject } from 'rxjs';
+import { Task } from '../models/Task';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,44 @@ export class UserService {
 
   execChange: Subject<any> = new Subject<any>();
 
+  totalHours: number = 0;
+
   constructor() { }
 
   changeUser(userChange: string) {
     this.execChange.next(userChange);
+  }
+
+  calculateTotalHours(tasks: Array<Task>): number {
+    let totalHours: number = 0;
+    
+    for (let task of tasks) {
+      totalHours += task.hours
+    }
+
+    this.totalHours = totalHours;
+
+    return totalHours
+  }
+
+  calculateUserLevel(tasks: Array<Task>): string {
+    let level: string;
+
+    console.log(tasks);
+
+    let hoursLogged: number = 0;
+
+    for (let task of tasks) {
+      hoursLogged += task.hours
+    };
+
+    console.log(hoursLogged);
+
+    return '';
+
+   /*  if (tasks < 10) {
+      level = 'Rookie';
+    } */
   }
 
 }
