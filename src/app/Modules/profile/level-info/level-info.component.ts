f139be239009786a6443a421f54e40aa8ebdddef
jakep5 <jakepagel1@yahoo.com>
2020-05-21 13:37:12 -0500

    implement inner experience bar

--- src/app/Modules/profile/level-info/level-info.component.ts
+++ src/app/Modules/profile/level-info/level-info.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../../shared/models/Task';
 import { UserService } from '../../shared/Services/user.service';
 import { TaskService } from '../../shared/Services/task-service.service';
+import { ProgressBar } from 'progress';
 
 @Component({
   selector: 'app-level-info',
@@ -9,6 +10,8 @@ import { TaskService } from '../../shared/Services/task-service.service';
   styleUrls: ['./level-info.component.css']
 })
 export class LevelInfoComponent implements OnInit {
+
+  @Input() userHours: number;
 
   constructor(
     private userService: UserService,
@@ -18,11 +21,36 @@ export class LevelInfoComponent implements OnInit {
 
   tasks: Array<Task>
   userLevel: string;
+  barTotal: string;
+
+  levelProgress: number;
+
 
   ngOnInit(): void {
     this.tasks = this.taskService.getTasks(sessionStorage.getItem('userId'))
       .then(tasks => this.userLevel = this.userService.calculateUserLevel(tasks))
+      .then(tasks => this.generateProgressBar())
   }
 
+  generateProgressBar () {
+    let barTotal;
+
+    console.log('here');
+
+    if (this.userLevel === 'novice') {
+      barTotal = 5;
+    } else if (this.userLevel === 'apprentice') {
+      barTotal = 30;
+    } else if (this.userLevel === 'adept') {
+      barTotal = 50;
+    } else if (this.userLevel === 'expert') {
+      barTotal = 100
+    } else {
+      barTotal = 1000;
+    }
+    
+    this.levelProgress = (this.userHours / barTotal) * 100;
+    
+  }
 
 }
--- src/app/Modules/profile/profile.module.ts
+++ src/app/Modules/profile/profile.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { ProfileNavComponent } from './profile-nav/profile-nav.component';
-import { ProgressbarComponent } from './progressbar/progressbar.component';
 import { LevelInfoComponent } from './level-info/level-info.component';
 
 
@@ -10,7 +9,7 @@ import { LevelInfoComponent } from './level-info/level-info.component';
 @NgModule({
   declarations: [
     ProfilePageComponent, 
-    ProfileNavComponent, ProgressbarComponent, LevelInfoComponent
+    ProfileNavComponent, LevelInfoComponent
   ],
   imports: [
     CommonModule
